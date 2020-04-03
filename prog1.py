f62aafc917a24f47c247ff75c23d5d43c99dde09
suraj6816 <haradagattisuraj@yahoo.co.in>
2020-04-03 12:37:16 -0400

    edited one line

--- prog1.py
+++ prog1.py
@@ -6,3 +6,5 @@ for i in range(1000, 3001):
     if (int(s[0])%2==0) and (int(s[1])%2==0) and (int(s[2])%2==0) and (int(s[3])%2==0):
         values.append(s)
 print ",".join(values)
+
+print('this is my first edit')
