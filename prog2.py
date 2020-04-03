fbc364061e5b06fa114a7f1ab77bafa69bc96d36
Suraj Haradagatti <36010530+suraj6816@users.noreply.github.com>
2020-04-03 12:28:51 -0400

    Add files via upload

---
+++ prog2.py
@@ -0,0 +1,16 @@
+#7 Write a program which takes 2 digits, X,Y as input and generates a 2-dimensional array.
+# The element value in the i-th row and j-th column of the array should be i*j.
+# Note: i=0,1.., X-1; j=0,1,¡­Y-1.
+
+input_str = '4,5'     #raw_input()
+dimensions=[int(x) for x in input_str.split(',')]
+rowNum=dimensions[0]
+colNum=dimensions[1]
+multilist = [[0 for col in range(colNum)] for row in range(rowNum)]
+print multilist
+
+for row in range(rowNum):
+    for col in range(colNum):
+        multilist[row][col]= row*col
+
+print multilist
